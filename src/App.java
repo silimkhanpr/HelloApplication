2b01fe9312c9c6fcd88175dd30b2e64f5b9fb540
Pranav-Silimkhan <pandurang_silimkhan@persistent.com>
2019-08-21 14:39:25 +0530

    I am not bored

--- src/App.java
+++ src/App.java
@@ -3,5 +3,6 @@ public class App
     public static void main(String[] args) {
         System.out.println("Hello Java");
         System.out.println("I am Pranav Silimkhan fggf");
+        System.out.println("Iam not bored of training");
     }
 }
